89242419e246a2bce2dcb45a80f38cd3f9f6f529
Mohamed Lazaar <lazzarmohamed10@gmail.com>
2026-02-15 11:25:55 +0000

    intial setup and project configuration for a django blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,4 @@
-from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -7,7 +7,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        User,
         on_delete=models.CASCADE,
         related_name='posts',
     )
